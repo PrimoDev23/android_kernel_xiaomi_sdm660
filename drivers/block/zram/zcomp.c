976850216efefbfc31fd8f40cb5f8090e0896118
Sergey Senozhatsky <sergey.senozhatsky.work@gmail.com>
2017-11-15 17:33:53 -0800

    zram: remove zlib from the list of recommended algorithms
    ZSTD tends to outperform deflate/inflate, thus we remove zlib from the
    list of recommended algorithms and recommend zstd instead.
    
    Link: http://lkml.kernel.org/r/20170912050005.3247-2-sergey.senozhatsky@gmail.com
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Suggested-by: Minchan Kim <minchan@kernel.org>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -23,9 +23,6 @@ static const char * const backends[] = {
 #ifdef CONFIG_ZRAM_LZ4_COMPRESS
 	"lz4",
 #endif
-#if IS_ENABLED(CONFIG_CRYPTO_DEFLATE)
-	"deflate",
-#endif
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4HC)
 	"lz4hc",
 #endif
