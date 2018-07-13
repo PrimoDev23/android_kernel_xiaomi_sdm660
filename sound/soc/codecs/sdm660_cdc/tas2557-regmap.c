bc478b134d20ad3fcd41e99772bd87c80df7cffd
ktommy91 <kemenesi.tamas@gmail.com>
2018-07-13 12:44:28 +0200

    ASoC:TAS2557 squashed update from TI upstream
    Signed-off-by: ktommy91 <kemenesi.tamas@gmail.com>

--- sound/soc/codecs/sdm660_cdc/tas2557-regmap.c
+++ sound/soc/codecs/sdm660_cdc/tas2557-regmap.c
@@ -1,8 +1,7 @@
 /*
 ** =============================================================================
 ** Copyright (c) 2016  Texas Instruments Inc.
-** Copyright (C) 2018 XiaoMi, Inc.
-**
+ * Copyright (C) 2018 XiaoMi, Inc.
 ** This program is free software; you can redistribute it and/or modify it under
 ** the terms of the GNU General Public License as published by the Free Software
 ** Foundation; version 2.
@@ -22,7 +21,7 @@
 
 #ifdef CONFIG_TAS2557_REGMAP
 
-
+#define DEBUG
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/init.h>
@@ -56,7 +55,6 @@
 #include "tiload.h"
 #endif
 
-
 #if defined(CONFIG_SND_SOC_TAS2557) && defined(CONFIG_SND_SOC_TFA98XX)
 static int btas2557 = 1;
 #endif
@@ -71,7 +69,7 @@ static int tas2557_change_book_page(
 {
 	int nResult = 0;
 
-	if ((pTAS2557->mnCurrentBook == nBook)
+	if ((pTAS2557->mnCurrentBook == nBook) 
 		&& pTAS2557->mnCurrentPage == nPage)
 		goto end;
 
@@ -139,7 +137,7 @@ static int tas2557_dev_read(
 				TAS2557_PAGE_REG(nRegister));
 	}
 
-	nResult = tas2557_change_book_page(pTAS2557,
+	nResult = tas2557_change_book_page(pTAS2557, 
 				TAS2557_BOOK_ID(nRegister),
 				TAS2557_PAGE_ID(nRegister));
 	if (nResult >= 0) {
@@ -272,7 +270,7 @@ static int tas2557_dev_bulk_write(
 				nLength);
 	}
 
-	nResult = tas2557_change_book_page(pTAS2557,
+	nResult = tas2557_change_book_page( pTAS2557,
 				TAS2557_BOOK_ID(nRegister),
 				TAS2557_PAGE_ID(nRegister));
 	if (nResult >= 0) {
@@ -313,7 +311,7 @@ static int tas2557_dev_update_bits(
 				nMask, nValue);
 	}
 
-	nResult = tas2557_change_book_page(pTAS2557,
+	nResult = tas2557_change_book_page( pTAS2557,
 				TAS2557_BOOK_ID(nRegister),
 				TAS2557_PAGE_ID(nRegister));
 	if (nResult >= 0) {
@@ -357,11 +355,9 @@ void tas2557_enableIRQ(struct tas2557_priv *pTAS2557, bool enable, bool startup_
 			}
 		}
 	} else {
-		if (pTAS2557->mbIRQEnable) {
-			if (gpio_is_valid(pTAS2557->mnGpioINT))
-				disable_irq_nosync(pTAS2557->mnIRQ);
-			pTAS2557->mbIRQEnable = false;
-		}
+		if (gpio_is_valid(pTAS2557->mnGpioINT))
+			disable_irq_nosync(pTAS2557->mnIRQ);
+		pTAS2557->mbIRQEnable = false;
 	}
 }
 
@@ -397,6 +393,9 @@ static void irq_work_routine(struct work_struct *work)
 #ifdef CONFIG_TAS2557_MISC
 	mutex_lock(&pTAS2557->file_lock);
 #endif
+
+	if(pTAS2557->mnErrCode & ERROR_FAILSAFE)
+		goto program;
 
 	if (pTAS2557->mbRuntimeSuspend) {
 		dev_info(pTAS2557->dev, "%s, Runtime Suspended\n", __func__);
@@ -595,8 +594,8 @@ static void timer_work_routine(struct work_struct *work)
 		if (!(pTAS2557->mnDieTvReadCounter % LOW_TEMPERATURE_COUNTER)) {
 			nAvg /= LOW_TEMPERATURE_COUNTER;
 			dev_dbg(pTAS2557->dev, "check : avg=%d\n", nAvg);
-			if ((nAvg & 0x80000000) != 0) {
-				/* if Die temperature is below ZERO */
+			if (nAvg < -6) {
+				/* if Die temperature is below -6 degree C */
 				if (pTAS2557->mnDevCurrentGain != LOW_TEMPERATURE_GAIN) {
 					nResult = tas2557_set_DAC_gain(pTAS2557, LOW_TEMPERATURE_GAIN);
 					if (nResult < 0)
@@ -764,6 +763,8 @@ static int tas2557_i2c_probe(struct i2c_client *pClient,
 	pTAS2557->hw_reset = tas2557_hw_reset;
 	pTAS2557->runtime_suspend = tas2557_runtime_suspend;
 	pTAS2557->runtime_resume = tas2557_runtime_resume;
+	pTAS2557->mnRestart = 0;
+	pTAS2557->mnEdge = 4;
 
 	mutex_init(&pTAS2557->dev_lock);
 
@@ -846,9 +847,7 @@ static int tas2557_i2c_probe(struct i2c_client *pClient,
 	nResult = request_firmware_nowait(THIS_MODULE, 1, pFWName,
 		pTAS2557->dev, GFP_KERNEL, pTAS2557, tas2557_fw_ready);
 
-	return nResult;
 err:
-
 #if defined(CONFIG_SND_SOC_TAS2557) && defined(CONFIG_SND_SOC_TFA98XX)
 	btas2557 = 0;
 #endif
@@ -879,7 +878,6 @@ static const struct i2c_device_id tas2557_i2c_id[] = {
 	{"tas2557", 0},
 	{}
 };
-
 
 #if defined(CONFIG_SND_SOC_TAS2557) && defined(CONFIG_SND_SOC_TFA98XX)
 int smartpa_is_tas2557(void)
@@ -917,7 +915,6 @@ module_i2c_driver(tas2557_i2c_driver);
 MODULE_AUTHOR("Texas Instruments Inc.");
 MODULE_DESCRIPTION("TAS2557 I2C Smart Amplifier driver");
 MODULE_LICENSE("GPL v2");
-
 
 #if defined(CONFIG_SND_SOC_TAS2557) && defined(CONFIG_SND_SOC_TFA98XX)
 EXPORT_SYMBOL(smartpa_is_tas2557);
