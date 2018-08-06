a2b26966cf8bc9af5062e742fd604f1ab85c8da2
feudofjustice <brandanhenderson07@gmail.com>
2018-08-06 18:43:30 -0400

    cpufreq:Introduce Darkutil-a combination of Helix + Electroutil.
    Signed-off-by: ktommy91 <kemenesi.tamas@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -524,6 +524,10 @@ void cpufreq_unregister_governor(struct cpufreq_governor *governor);
  */
 #ifdef CONFIG_CPU_FREQ_GOV_PERFORMANCE
 extern struct cpufreq_governor cpufreq_gov_performance;
+#elif CONFIG_CPU_FREQ_GOV_DARKUTIL
+extern struct cpufreq_governor cpufreq_gov_darkutil;
+#elif CONFIG_CPU_FREQ_GOV_SCHEDUTIL
+extern struct cpufreq_governor cpufreq_gov_schedutil;
 #endif
 #ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_performance)
@@ -548,7 +552,11 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKUTIL)
+extern struct cpufreq_governor cpufreq_gov_darkutil;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkutil)
 #endif
+
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
 {
