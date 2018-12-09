4636ade60e1b684ccd7a36fcafd82161ba0979f4
kdrag0n <dragon@khronodragon.com>
2018-12-08 23:07:34 -0800

    cpu_input_boost: only boost on app launch if recently touched
    This fixes unnecessary background boosting (especially right after boot)
    of services, content providers, and other background processes launching
    as zygote. Max zygote boost will only occur within 150 ms of the latest
    input event.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1812,6 +1812,14 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
+#ifdef CONFIG_CPU_INPUT_BOOST
+	/* Boost CPU to the max for 1250 ms when userspace launches an app */
+	if (is_zygote_pid(current->pid) && !state_suspended &&
+		time_before(jiffies, last_input_time + msecs_to_jiffies(150))) {
+		cpu_input_boost_kick_max(1250);
+	}
+#endif
+
 	/*
 	 * Determine whether and which event to report to ptracer.  When
 	 * called from kernel_thread or CLONE_UNTRACED is explicitly
