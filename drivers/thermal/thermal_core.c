f270b61ec9d60ec3791e1a8affae1f1b7143c801
Adek Maulana <adek@techdro.id>
2019-02-14 05:14:05 +0700

    msm: thermal: if checked India version, set diff value
    Change-Id: I25e56edf8fd0eba2a2d3cacce37297ebceeea496
    Signed-off-by: YousefAlgadri <yusufgadrie@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -2781,14 +2781,28 @@ thermal_sconfig_show(struct device *dev,
 	return snprintf(buf, PAGE_SIZE, "%d\n", atomic_read(&switch_mode));
 }
 
+#ifdef CONFIG_KERNEL_CUSTOM_WHYRED
+extern int hwc_check_india;
+#endif
 static ssize_t
 thermal_sconfig_store(struct device *dev,
 				      struct device_attribute *attr, const char *buf, size_t len)
 {
 	int val = -1;
+#ifdef CONFIG_KERNEL_CUSTOM_WHYRED
+	struct thermal_zone_device *tz = to_thermal_zone(dev);
+#endif
 
 	val = simple_strtol(buf, NULL, 10);
 
+#ifdef CONFIG_KERNEL_CUSTOM_WHYRED
+	if (0 == val) {
+		if (1 == hwc_check_india)
+			val = 2;
+	}
+
+	dev_err(&tz->device, "hwc_check_india=%d, set sconfig to %d", hwc_check_india, val);
+#endif
 	atomic_set(&switch_mode, val);
 
 	return len;
