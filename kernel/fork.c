a0739d1ce8f2f41516bba6f2126006f04351ab11
kdrag0n <dragon@khronodragon.com>
2019-02-20 19:44:23 -0800

    kernel: Only boost for 32 ms on zygote fork
    This will work as a short boost to cover the time between zygote forking
    and the PID being added to top-app cgroups.
    
    In testing, the delay was usually less than 25 ms, so 32 ms is
    sufficient.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1815,10 +1815,10 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 #ifdef CONFIG_CPU_INPUT_BOOST
-	/* Boost CPU to the max for 1250 ms when userspace launches an app */
+	/* Boost CPU to the max for 32 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid) && !state_suspended &&
-		time_before(jiffies, last_input_jiffies + msecs_to_jiffies(150))) {
-		cpu_input_boost_kick_max(1250);
+		time_before(jiffies, last_input_jiffies + msecs_to_jiffies(75))) {
+		cpu_input_boost_kick_max(32);
 	}
 #endif
 
