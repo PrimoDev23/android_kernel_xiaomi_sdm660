a63e169d3e3becb32a14d748352ccf0944726e8d
PrimoDev23 <lexx.ps2711@gmail.com>
2019-02-25 14:43:55 +0100

    mdss: Boost CPU when commit a new frame

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -51,6 +51,7 @@
 #include <linux/wakelock.h>
 #include <sync.h>
 #include <sw_sync.h>
+#include <linux/cpu_input_boost.h>
 
 #include "mdss_dsi.h"
 #include "mdss_fb.h"
@@ -5590,6 +5591,7 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
+		cpu_input_boost_kick_general(100);
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
