49049ee5154199d5718c7af2f890f408ba73df1f
PrimoDev23 <lexx.ps2711@gmail.com>
2019-03-22 09:56:25 +0100

    use should boost_frame_function instead of a hardcoded time_before on app launch
    this cuased not really working max boost on app launch

--- kernel/fork.c
+++ kernel/fork.c
@@ -1821,7 +1821,7 @@ long _do_fork(unsigned long clone_flags,
 #ifdef CONFIG_CPU_INPUT_BOOST
 	/* Boost CPU to the max for 32 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid) && !state_suspended &&
-		time_before(jiffies, last_input_jiffies + msecs_to_jiffies(75))) {
+		cpu_input_boost_should_boost_frame()) {
 		pr_info("Max Boost on App launch");
 		cpu_input_boost_kick_max(32);
 	}
