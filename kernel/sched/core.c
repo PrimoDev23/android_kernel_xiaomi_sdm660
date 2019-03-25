07b1037b2920560e7b2659ec306a83433824e59b
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-25 13:23:28 -0700

    kernel: Force trivial, unbound kthreads onto low-power CPUs
    In order to reduce power consumption, force all non-perf-critical,
    unbound kthreads onto the low-power CPUs. Note that init must be
    explicitly excluded from this so that all processes forked from init
    have a sane default CPU affinity mask.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1219,8 +1219,8 @@ void set_cpus_allowed_common(struct task_struct *p, const struct cpumask *new_ma
 	p->nr_cpus_allowed = cpumask_weight(new_mask);
 }
 
-static const struct cpumask *get_adjusted_cpumask(const struct task_struct *p,
-	const struct cpumask *req_mask)
+static const struct cpumask *
+get_adjusted_cpumask(struct task_struct *p, const struct cpumask *orig_mask)
 {
 	/* Force all performance-critical kthreads onto the big cluster */
 	if (p->flags & PF_PERF_CRITICAL)
@@ -1229,7 +1229,12 @@ static const struct cpumask *get_adjusted_cpumask(const struct task_struct *p,
 	if (p->flags & PF_LOW_POWER)
 		return cpu_lp_mask;
 
-	return req_mask;
+	/* Force all trivial, unbound kthreads onto the little cluster */
+	if (p->flags & PF_KTHREAD && !is_global_init(p) &&
+	    cpumask_equal(orig_mask, cpu_all_mask))
+		return cpu_lp_mask;
+
+	return orig_mask;
 }
 
 void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
