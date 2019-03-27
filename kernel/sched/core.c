a15de66f64b6e263686c9e346e3d0afc11ecd929
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-27 10:57:14 -0700

    Revert "kernel: Force trivial, unbound kthreads onto low-power CPUs"
    Unfortunately, the overhead of kthreads is too much for our poor little
    cluster to keep up. The scheduler will try to put kthreads onto the
    little cluster anyway, so this isn't needed.
    
    This reverts commit 33ff5c27f4504bd58186d42317918276a6283150.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1219,8 +1219,8 @@ void set_cpus_allowed_common(struct task_struct *p, const struct cpumask *new_ma
 	p->nr_cpus_allowed = cpumask_weight(new_mask);
 }
 
-static const struct cpumask *
-get_adjusted_cpumask(struct task_struct *p, const struct cpumask *orig_mask)
+static const struct cpumask *get_adjusted_cpumask(const struct task_struct *p,
+	const struct cpumask *req_mask)
 {
 	/* Force all performance-critical kthreads onto the big cluster */
 	if (p->flags & PF_PERF_CRITICAL)
@@ -1229,12 +1229,7 @@ get_adjusted_cpumask(struct task_struct *p, const struct cpumask *orig_mask)
 	if (p->flags & PF_LOW_POWER)
 		return cpu_lp_mask;
 
-	/* Force all trivial, unbound kthreads onto the little cluster */
-	if (p->flags & PF_KTHREAD && !is_global_init(p) &&
-	    cpumask_equal(orig_mask, cpu_all_mask))
-		return cpu_lp_mask;
-
-	return orig_mask;
+	return req_mask;
 }
 
 void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
