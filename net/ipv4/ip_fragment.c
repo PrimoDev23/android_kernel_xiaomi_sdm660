c8d44ad476d173f9ad3f4dc964f9133b5bfc8c7a
PrimoDev23 <lexx.ps2711@gmail.com>
2019-05-02 11:32:53 +0200

    ipv4: ip_fragment: Remove unused function
    Since I reverted 2 upstream commits this isn't needed. Whyever I have left this
    in for so long time but now it's time to cleanup

--- net/ipv4/ip_fragment.c
+++ net/ipv4/ip_fragment.c
@@ -74,14 +74,6 @@ struct ipfrag_skb_cb {
 
 #define FRAG_CB(skb)		((struct ipfrag_skb_cb *)((skb)->cb))
 
-static void ip4_frag_init_run(struct sk_buff *skb)
-{
-	BUILD_BUG_ON(sizeof(struct ipfrag_skb_cb) > sizeof(skb->cb));
-
-	FRAG_CB(skb)->next_frag = NULL;
-	FRAG_CB(skb)->frag_run_len = skb->len;
-}
-
 /* Describe an entry in the "incomplete datagrams" queue. */
 struct ipq {
 	struct inet_frag_queue q;
